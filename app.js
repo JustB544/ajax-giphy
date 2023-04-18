f2f2e3adbbdcee147ecfb610f0772fdfd0a98f69
JustB544 <breytonpabst@gmail.com>
2023-04-17 21:13:44 -0400

    initial

---
+++ app.js
@@ -0,0 +1,28 @@
+const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+const form = document.querySelector("form");
+const gifs = document.querySelector("#gifs");
+const remove = document.querySelector("#remove");
+const search = document.querySelector("#inputs form input");
+
+remove.addEventListener("click", () => {
+    gifs.innerHTML = "";
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchGiphy(search.value);
+    search.value = "";
+});
+
+async function searchGiphy(search) {
+    const results = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: search, api_key } });
+    const giphyImg = results.data.data[0].images.original.url;
+    //console.log(results.data.data[0]);
+
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = giphyImg;
+    div.append(img);
+    gifs.append(div);
+}
